2276e82a7d3faa96b98b117966a9bfbdf991cb15
admin <admin@openoss1.localdomain>
2018-03-16 14:52:32 +0000

    first commit of mqtt laser

--- ldr2.py
+++ ldr2.py
@@ -6,6 +6,7 @@ from gpiozero import LightSensor, Buzzer #import LDR and Buzzer from GPIO pin ze
 from time import sleep
 from time import strftime
 import datetime #Imports the date and time in to file
+import paho.mqtt.publish as publish
 
 ldr = LightSensor(4)
 buzzer = Buzzer(17)
@@ -17,8 +18,10 @@ while True: #continuously checks the light level on LDR
         if ldr.value < 0.8: #if LDR value falls below 0.5 buzzer will
             buzzer.beep(4) #how long the buzzer beeps for
             f.write(timestamp+","), #inserts time stamp in to analytics
-            f.write("1"+","),       #Inserts a one in to the analytics to show buzzer had sounded
+            f.write("1"+","),       #Insertsmqttproject a one in to the analytics to show buzzer had sounded
             f.write(str(ldr)+"\n"),
+            
+            publish.single("mqtt-events", ldr.value, hostname="test.mosquitto.org")
         else:
             buzzer.off()
             f.write(timestamp +","),
